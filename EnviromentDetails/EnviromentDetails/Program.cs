8081ed52f4e6b17c089912648fdd776cae502c4f
Owl <ad-2812@yandex.com>
2016-02-26 15:30:06 +0300

    Added files via upload

---
+++ EnviromentDetails/EnviromentDetails/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace EnviromentDetails
+{
+    class Program
+    {
+        static void Main(string[] args)
+         {
+            ShowEnviromentDateils();
+
+            Console.ReadLine();
+            
+        }
+
+
+        static void ShowEnviromentDateils()
+            {
+                Console.Title = "System computer information";
+                Console.SetWindowSize(90, 40);
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("------------System info--------------- ");
+                Console.WriteLine("Computer's name: {0} ", Environment.MachineName);
+                Console.WriteLine("OS: {0} ", Environment.OSVersion);
+                Console.WriteLine("Version OS: {0} ", Environment.Version);
+                Console.WriteLine("Is x64?: {0}", Environment.Is64BitOperatingSystem);
+                Console.WriteLine("Number of processors: {0} ", Environment.ProcessorCount);
+                Console.WriteLine("System directory: {0} ", Environment.SystemDirectory);
+                Console.WriteLine("Acive user's session: {0} ", Environment.UserName);
+                
+               foreach (string drive in Environment.GetLogicalDrives())
+                    Console.WriteLine("Drive: {0}", drive);
+               Console.WriteLine("------------End system info-----------");
+                
+            }
+        static void ChangeConsoleDesign()
+        {
+            
+        }
+    }
+}
