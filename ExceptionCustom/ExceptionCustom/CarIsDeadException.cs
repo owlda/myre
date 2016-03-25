aa43ab17fffecab01b5790037fe4241d76448d29
Owl <ad-2812@yandex.com>
2016-03-25 15:49:24 +0300

    Added files via upload

---
+++ ExceptionCustom/ExceptionCustom/CarIsDeadException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExceptionCustom
+{
+    class CarIsDeadException: ApplicationException
+    {
+        private string messageDetails = string.Empty;
+        public DateTime ErrorTimeStamp { get; set; }
+        public string CauseOfError { get; set; }
+
+        public CarIsDeadException() { }
+        public CarIsDeadException(string message, string cause, DateTime datetime) 
+        {
+            messageDetails = message;
+            ErrorTimeStamp = datetime;
+            CauseOfError = cause;
+        }
+        public override string Message
+        {
+            get
+            {
+
+                return string.Format("Car Error Massage: {0}", messageDetails);
+            }
+        }
+
+    }
+}
---
+++ ExceptionCustom/ExceptionCustom/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExceptionCustom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car007 = new Car("Volvo", "Black", "John" ,0);
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    car007.Accelerate(10);
+                }
+            }
+            catch (CarIsDeadException e) 
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ErrorTimeStamp);
+                Console.WriteLine(e.CauseOfError);
+            }
+        }
+    }
+}
