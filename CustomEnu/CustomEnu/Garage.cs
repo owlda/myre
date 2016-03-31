32643ace7896bab51437cebd0e89284482c00d47
Owl <ad-2812@yandex.com>
2016-03-31 16:01:07 +0300

    Added files via upload

---
+++ CustomEnu/CustomEnu/Garage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace CustomEnu
+{
+    public class Garage: IEnumerable
+    {
+        private Car[] CarsInGarage = new Car[4];
+        public Garage() 
+        {
+            CarsInGarage[0] = new Car("Ford","DarkGreen","Rusty",0);
+            CarsInGarage[1] = new Car("Renault", "DarkRed", "Bony", 0);
+            CarsInGarage[2] = new Car("Mitsubishi", "Green", "Cookie", 0);
+            CarsInGarage[3] = new Car("Chevrolet", "White", "Don", 0);
+        }
+        public IEnumerator GetEnumerator() { return CarsInGarage.GetEnumerator(); }
+    }
+}
---
+++ CustomEnu/CustomEnu/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace CustomEnu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Garage myga = new Garage();
+            foreach (Car i in myga)
+            {
+                Console.WriteLine(i);
+            }
+            IEnumerator enu = myga.GetEnumerator();
+            enu.Reset();
+            }
+    }
+}
