4e0b90ec6a4dd7d66eb6d4109da07a6e4af3a672
Owl <ad-2812@yandex.com>
2016-09-30 17:25:47 +0300

    Add files via upload
    add item of list with fixed id in the list.

---
+++ AddListItem/AddListItem/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint;
+
+namespace AddItemList
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                using (SPSite site = new SPSite("http://sharepointsrv/sites/portal"))
+                {
+                    using (SPWeb web = site.OpenWeb())
+                    {
+                        SPList list = web.GetList("/Lists/MemosLst/");
+                        SPListItemCollection items = list.Items;
+
+                        SPListItem item = items.Add();
+                        item[SPBuiltInFieldId.Title] = "Новая служебная записка";
+                        item[SPBuiltInFieldId.Body] = "Создана для исправления ошибочного удаления элемента.";
+                        item[SPBuiltInFieldId.Expires] = DateTime.Now.AddHours(1);
+                        item[SPBuiltInFieldId.ID] = 10608;
+                        item.Update();
+                        
+                        Guid id = item.UniqueId;
+                        
+                    Console.WriteLine(items[id].Title);
+                    }
+                }
+                Console.ReadLine();
+            }
+        }
+    }
