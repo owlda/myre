9689eac90175490b196126309a4ef33e9d4bce43
Owl <ad-2812@yandex.com>
2016-11-25 12:44:34 +0300

    Add files via upload
    Visual WebPart for finding List URI and redirecting.

---
+++ FindFullURLList/VisualWebPart99/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="be72c9f6-484f-47b4-9a1e-93da24daaa5f" featureId="be72c9f6-484f-47b4-9a1e-93da24daaa5f" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="VisualWebPart99 Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="719ac04e-3ad9-4fa8-a460-2eda356c800f" />
+  </projectItems>
+</feature>
---
+++ FindFullURLList/VisualWebPart99/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// Общие сведения о сборке можно задать с помощью следующего 
+// набора атрибутов. Отредактируйте эти значения атрибутов, чтобы изменить сведения,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("VisualWebPart99")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("VisualWebPart99")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// При установке значения False в атрибуте ComVisible типы в этой сборке становятся невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте для атрибута ComVisible значение True для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект видим для COM
+[assembly: Guid("f50bb397-49b1-407b-a8cd-3eb1c37a1e29")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
